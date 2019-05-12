1af5abe65ddd4ef23a1d5599ff10dab03dde7850
WRup <wiktor-rup@wp.pl>
2019-05-12 15:34:53 +0200

    added model classes which represents main battleship game entities

---
+++ src/main/java/wkbp/battleship/model/Compass.java
@@ -0,0 +1,10 @@
+package wkbp.battleship.model;
+
+/**
+ * Enum class which represents possible directions of ship spacing.
+ * @see wkbp.battleship.model.Ship
+ * @author Wiktor Rup
+ */
+public enum Compass {
+    HORIZONTAL, VERTICAL
+}
