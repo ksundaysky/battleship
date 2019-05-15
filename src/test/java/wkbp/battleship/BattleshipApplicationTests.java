0ac76443726ae3a39bb9d4654a4fe40fceca8c89
twojatata <patrykkuchar@gmail.com>
2019-05-15 16:17:44 +0200

    fix BattleShipApp test

--- src/test/java/wkbp/battleship/BattleshipApplicationTests.java
+++ src/test/java/wkbp/battleship/BattleshipApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import wkbp.battleships.SpringBootJwtAuthenticationApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpringBootJwtAuthenticationApplication.class)
+@SpringBootTest()
 public class BattleshipApplicationTests {
 
     @Test
